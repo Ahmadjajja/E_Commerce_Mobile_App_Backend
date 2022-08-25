bee4db0161f4b490cdf6025d77b99c624ff322cf
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2022-08-25 12:10:15 +0500

    backend ready for deployment

--- app.js
+++ app.js
@@ -47,7 +47,13 @@ mongoose
     console.log(err);
   });
 
-//Server
-app.listen(3000, () => {
-  console.log("server is running http://localhost:3000");
-});
+//Development Server
+// app.listen(3000, () => {
+//   console.log("server is running http://localhost:3000");
+// });
+
+//Production Server
+var server = app.listen(process.env.PORT || 3000, function () {
+  var port = server.address().port;
+  console.log("Express is working on port " + port)
+})
